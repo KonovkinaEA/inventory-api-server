701e14b10ed014b5129ccce2b599433358b79be0
liz(s)a <el.konovkina@mail.ru>
2024-04-26 16:48:53 +0300

    Initial commit

---
+++ src/main/java/com/example/inventoryapiserver/InventoryApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventoryapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inventoryapiserver/InventoryApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventoryapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
